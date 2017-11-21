10414aedbc4ea8ed7edc5ff157f37b0af853913c
Szabolcs Kiss <32358310+szabolcskissmail@users.noreply.github.com>
2017-11-21 21:58:16 +0100

    bitset

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
+#include <bitset>
 
 using namespace std;
 
+#define KT153_BITS 360
+
 class kt153 {
+private:
+    std::bitset<KT153_BITS> bits;
+public:
+    typedef unsigned char kt153_type;
+    kt153(const kt153_type init) : bits(){
+        std::bitset<8> init_bitset (init);
+        for(unsigned int i = 0; i < init_bitset.size(); i++){
+            bits[i] = init_bitset[i];
+        }
+    }
 };
 
 int main()
 {
-    cout << "kt153" << endl;
+    std::cout << "kt153" << endl;
     return 0;
 }
