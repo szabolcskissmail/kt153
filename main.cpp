cf16837beaf394fb7ad286a641e83536c68ccf39
Szabolcs Kiss <32358310+szabolcskissmail@users.noreply.github.com>
2017-11-22 22:19:03 +0100

    Operators

--- main.cpp
+++ main.cpp
@@ -4,22 +4,44 @@
 using namespace std;
 
 #define KT153_BITS 360
+#define KT153_VALUE_BITS 8
 
-class kt153 {
+class kt153
+{
 private:
+    typedef unsigned int pointer_type;
     std::bitset<KT153_BITS> bits;
+    pointer_type pointer;
 public:
     typedef unsigned char kt153_type;
-    kt153(const kt153_type init) : bits(){
-        std::bitset<8> init_bitset (init);
-        for(unsigned int i = 0; i < init_bitset.size(); i++){
-            bits[i] = init_bitset[i];
+    kt153(const kt153_type init) : bits()
+    {
+        pointer = 0;
+        std::bitset<KT153_VALUE_BITS> init_bitset (init);
+        unsigned int j = 0;
+        for(unsigned int i = 0; i < bits.size(); i++)
+        {
+            bits[i] = init_bitset[j++];
+            if(j == KT153_VALUE_BITS - 1)
+            {
+                j = 0;
+            }
         }
+    }
+    void operator++()
+    {
+        pointer++;
+    }
+    void operator++(int)
+    {
+        operator++();
     }
 };
 
 int main()
 {
     std::cout << "kt153" << endl;
+    kt153 k (255);
+    k++;
     return 0;
 }
