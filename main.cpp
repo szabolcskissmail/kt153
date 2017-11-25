e6ca501eb52e40aedd55aedcb2a01dfc426d232f
Szabolcs Kiss <32358310+szabolcskissmail@users.noreply.github.com>
2017-11-25 09:36:33 +0100

    value calculation

--- main.cpp
+++ main.cpp
@@ -6,8 +6,7 @@ using namespace std;
 #define KT153_BITS 360
 #define KT153_VALUE_BITS 8
 
-class kt153
-{
+class kt153 {
 private:
     typedef unsigned int pointer_type;
     std::bitset<KT153_BITS> bits;
@@ -19,18 +18,33 @@ public:
         pointer = 0;
         std::bitset<KT153_VALUE_BITS> init_bitset (init);
         unsigned int j = 0;
-        for(unsigned int i = 0; i < bits.size(); i++)
-        {
+        for(unsigned int i = 0; i < bits.size(); i++) {
             bits[i] = init_bitset[j++];
-            if(j == KT153_VALUE_BITS - 1)
-            {
+            if(j == KT153_VALUE_BITS - 1) {
                 j = 0;
             }
         }
     }
+    kt153_type value()
+    {
+        std::bitset<KT153_VALUE_BITS> return_bitset (0);
+        unsigned int normalized_pointer = pointer;
+        for (unsigned int i = 0; i < KT153_VALUE_BITS; i++) {
+            if(normalized_pointer == KT153_BITS) {
+                normalized_pointer = 0;
+            }
+            return_bitset[i] = bits[normalized_pointer];
+            normalized_pointer++;
+        }
+        const unsigned long i = return_bitset.to_ulong();
+        return static_cast<kt153_type>(i); ;
+    }
     void operator++()
     {
         pointer++;
+        if(pointer == KT153_BITS){
+            pointer = 0;
+        }
     }
     void operator++(int)
     {
@@ -41,7 +55,10 @@ public:
 int main()
 {
     std::cout << "kt153" << endl;
-    kt153 k (255);
-    k++;
+    kt153 k (135);
+    for (int i = 0; i < 500; i++) {
+        std::cout << "value " << static_cast<unsigned>(k.value()) << endl;
+        k++;
+    }
     return 0;
 }
